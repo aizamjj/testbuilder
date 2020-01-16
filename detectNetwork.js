5e8aff63cc434ccb02048179d95580b43bd28c18
Aizam Jigjidsuren <aizamjig@gmail.com>
2020-01-16 13:25:37 -0800

    Step Three: added two more card types, Discover and Maestro into the network array; created two more conditionals for different lengths of prefixes.

--- detectNetwork.js
+++ detectNetwork.js
@@ -15,6 +15,9 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
   // Visa always has a prefix of 4 and a length of 13, 16, or 19.
   // MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
+  // Discover always has a prefix of 6011, 644-649, or 65, and a length of 16 or 19.
+  // Maestro always has a prefix of 5018, 5020, 5038, or 6304, and a length of 12-19.
+
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
   //Pseudocode:
@@ -28,14 +31,22 @@ var detectNetwork = function(cardNumber) {
     prefix: ["38", "39"],
     lengths: [14]
   },{
-    name: "Visa",       // card issuer (network)
-  prefix: ["4"],   // beginning digits
-  lengths: [13, 16, 19]  // lengths of card numbers
-},{
-  name: "MasterCard",
-  prefix: ["51", "52", "53", "54", "55"],
-  lengths: [16]
-}];
+    name: "Visa",      
+    prefix: ["4"],   
+    lengths: [13, 16, 19]
+  },{
+    name: "MasterCard",
+    prefix: ["51", "52", "53", "54", "55"],
+    lengths: [16]
+  },{
+    name: "Discover",
+    prefix: ["6011", "644", "645", "646", "647", "648", "649", "65"],
+    lengths: [16, 19]
+  },{
+    name: "Maestro",
+    prefix: ["5018", "5020", "5038", "6304"],
+    lengths: [12, 13, 14, 15, 16, 17, 18, 19]
+  }];
 
   //iterate through the array of objects 
   for (var i = 0; i < network.length; i++) {
@@ -50,7 +61,16 @@ var detectNetwork = function(cardNumber) {
      if (cardNumber.slice(0, 2) === pre && cardNumber.length === lengths) { 
        return nwrk;
      }
+     // if the first digit is '4' and the length matches then it is Visa
      if (cardNumber[0] === pre && cardNumber.length === lengths) {
+       return nwrk;
+     }
+     // if the first four digits match and the lengths match then it is Maestro
+     if (cardNumber.slice(0, 4) === pre && cardNumber.length === lengths) {
+       return nwrk;
+     }
+     // if the first three digits and the lengths match then it is Discover
+     if (cardNumber.slice(0, 3) === pre && cardNumber.length === lengths) {
        return nwrk;
      }
    }
