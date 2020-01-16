dc90cdabadeb6962fa17f1728e76639acddf0dfa
Aizam Jigjidsuren <aizamjig@gmail.com>
2020-01-16 10:37:49 -0800

    Previous commit did not pass all the tests on the browser (found out it was due to spelling MasterCard with lower-case 'c') Changed the solution completely: instead of conditionals, it is now looping over the newly created network array object to look up the proper network name.

--- detectNetwork.js
+++ detectNetwork.js
@@ -9,50 +9,51 @@
 
 // input: cardNumber - string;
 // output: card network name - string
-
 var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
+  // Visa always has a prefix of 4 and a length of 13, 16, or 19.
+  // MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
   //Pseudocode:
-  //create a length variable
-  var len = cardNumber.length;
-  //create a first digit variable
-  var first = cardNumber[0];
-  //create a second digit variable
-  var second = cardNumber[1];
-  //if the first digit is 4 and length is 13, 16 or 19
-  if (first === '4') {
-  	if (len === 13 || len === 16 || len === 19) {
-  		//return visa
-  		return 'Visa';
-  	}
-  //if the first digit is 5 and the second digits are 1-5, and length is 16
-  } else if (first === '5') {
-  	if (len === 16 && second === '1' || second === '2' || second === '3' || second === '4' || second === '5') {
-  		//return mastercard
-  		return 'Mastercard';
-  	}  
-  // if the first digits are 3, then
-  } else {
-    if (first === '3') {
-  	//if the second digits are equal to 8 or 9 and the lengths are 14
-  	  if (len === 14 && second === '8' || second === '9') {
-  		//return diner's club
-  		return 'Diner\'s Club';
-  	}
-    //if the second digits are equal to 4 or 7 and the lengths are 15
-    if (len === 15 && second === '4' || second === '7') {
-    	//return amex 
-    	return 'American Express';
-    }
-  }
-  }
+  // create a network array with card objects: name, length, and prefix
+  var network = [{
+    name: "American Express",
+    prefix: ["34", "37"],
+    lengths: [15]
+  },{
+    name: "Diner's Club",
+    prefix: ["38", "39"],
+    lengths: [14]
+  },{
+    name: "Visa",       // card issuer (network)
+  prefix: ["4"],   // beginning digits
+  lengths: [13, 16, 19]  // lengths of card numbers
+},{
+  name: "MasterCard",
+  prefix: ["51", "52", "53", "54", "55"],
+  lengths: [16]
+}];
+
+  //iterate through the array of objects 
+  for (var i = 0; i < network.length; i++) {
+   // iterate through the prefixes
+   for (var j = 0; j < network[i].prefix.length; j++) {
+     // iterate through the lengths
+     for (var k = 0; k < network[i].lengths.length; k++) {
+     var pre = network[i].prefix[j];
+     var lengths = network[i].lengths[k];
+     var nwrk = network[i].name;
+     // check to see if the first two digits are a match and if the lengths are a match
+     if (cardNumber.slice(0, 2) === pre && cardNumber.length === lengths) { 
+       return nwrk;
+     }
+     if (cardNumber[0] === pre && cardNumber.length === lengths) {
+       return nwrk;
+     }
+   }
+ }
+}
 };
-
-detectNetwork('343456789012345');
-
-
-
